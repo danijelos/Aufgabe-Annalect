303fd6065205bf53d9c672f143356f41b0c5aa00
danijelos <daniel.becker@alumni.fh-aachen.de>
2024-03-29 10:49:41 +0100

    Created a flask backend with a placeholder function x^2 in flask_backend.py; Created the flask call testwise in a jupyter notebook

---
+++ flask_backend.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+class Modeler:
+    def predict(self, x):
+        return x**2
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def post():
+    data = request.get_json()
+    x = data['x']
+    m = Modeler()
+    prediction = m.predict(x)
+    return jsonify({
+        "Input": x,
+        "y": prediction
+    })
+
+if __name__ == "__main__":
+    ## Uncomment for flask only (no docker container)
+    app.run(port=5000,debug=True)
+    ## Comment out for flask only (no docker container)
+    # app.run(host="0.0.0.0", port=8080)
